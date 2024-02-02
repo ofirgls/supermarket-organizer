31b3619ffb9a82e0444a45812ad222445f5143e8
Ofir <ofirgls5588@gmail.com>
2024-02-02 18:38:28 +0200

    routes

--- backend/app.js
+++ backend/app.js
@@ -3,21 +3,17 @@ const path = require('path');
 const http = require('http');
 const app = express();
 
-//middleware
 
+const {routeInit} = require("./routes/config_route");
+
+//middleware
 /*Jason information parsering definition*/
 app.use(express.json());
-
 /*Setting the public folder as a static folder where files can be put and the
 client side will have access to it */
 app.use(express.static(path.join(__dirname,"public")));
 
-
-app.get('/', (req, res)=>{
-    res.json({
-        massage:'hello worldddd',
-    })
-});
+routeInit(app);
 
 const server = http.createServer(app);
 
