df14948928ca260e5f982d858379fd14bd385b81
Ofir <ofirgls5588@gmail.com>
2024-02-07 21:49:46 +0200

    product list

--- frontend/src/components/CurrentList.js
+++ frontend/src/components/CurrentList.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
 import './css/CurrentList.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeColor } from '../store/marketSlice';
 
 const CurrentList = () => {
   const marketList = useSelector((state) => state.market.marketList);
   const categories = [...new Set(marketList.map((product) => product.category))];
+  const [clickedProducts, setClickedProducts] = useState({});
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const initialClickedProducts = {};
+    marketList.forEach(product => {
+      initialClickedProducts[product.id] = product.color || 'red'; 
+    });
+    setClickedProducts(initialClickedProducts);
+  }, [marketList]); 
+
+  const toggleColor = (productId) => {
+    const updatedClickedProducts = {
+      ...clickedProducts,
+      [productId]: clickedProducts[productId] === 'red' ? 'green' : 'red',
+    };
+    setClickedProducts(updatedClickedProducts);
+    const color = updatedClickedProducts[productId];
+    dispatch(changeColor({ productId, color }));
+  };
 
   return (
     <div className='list_container'>
@@ -15,7 +36,14 @@ const CurrentList = () => {
             <h2>{category}:</h2>
             <ul className='products'>
               {marketList.filter((product) => product.category === category).map((product, subIndex) => (
-                <li key={subIndex}>{product.name}</li>
+                <li key={subIndex}>
+                  <button
+                    className={`buy ${clickedProducts[product.id]}`}
+                    onClick={() => toggleColor(product.id)}
+                  >
+                    {product.name}
+                  </button>
+                </li>
               ))}
             </ul>
           </li>
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -6,13 +6,18 @@ import './css/HomePage.css';
 const HomePage = () => {
   const [productName, setProductName] = useState('');
   const [productCategory, setProductCategory] = useState('');
-  
+  const [categories, setCategories] = useState(["fruits", "vegetables", "cheeses", "drinks", "snacks"]);
 
   const dispatch = useDispatch();
 
   const addNewProduct = () => {
-    console.log("productName", { productName }, "productCategory", { productCategory });
-    const payload = { name: productName, category: productCategory };
+    if (!categories.includes(productCategory)) {
+      setCategories([...categories, productCategory]);
+    }
+    console.log(categories);
+    const randomId = Math.floor(Math.random() * 10000000000);
+    console.log("productName: ", productName, "productCategory: ", productCategory, "randomId: ", randomId);
+    const payload = { name: productName, category: productCategory, id: randomId, color: "red"};
     dispatch(addProduct(payload));
     setProductName('');
     setProductCategory('');
@@ -21,7 +26,7 @@ const HomePage = () => {
   return (
     <div className='home_page_container'>
       <div className='wellcome'>
-        <h1>Wellcome to the market list organizer</h1>
+        <h1>Welcome to the market list organizer</h1>
       </div>
       <form className='market_form' onSubmit={(e) => { e.preventDefault(); addNewProduct(); }}>
         <h2>Add your products to the list:</h2>
@@ -38,15 +43,41 @@ const HomePage = () => {
             required
           />
           <br />
-          <label htmlFor='productCategory'>Product category:</label>
-          <input
-            type='text'
-            id='productCategory'
-            name='productCategory'
-            value={productCategory}
-            onChange={(e) => setProductCategory(e.target.value)}
-            required
-          />
+          {(productCategory === "" && productCategory !== "new category") || categories.includes(productCategory) ? (
+            <div>
+              <label htmlFor='productCategory' style={{ marginBottom: "20px" }}>Product category:</label>
+              <br />
+              <select
+                id='productCategorySelect'
+                name='productCategory'
+                value={productCategory}
+                onChange={(e) => setProductCategory(e.target.value)}
+                required
+                style={{ marginTop: "20px" }}
+              >
+                <option value="">Select Category</option>
+                {categories.map((category, index) => (
+                  <option key={index} value={category}>{category}</option>
+                ))}
+                <option value="new category">new category</option>
+              </select>
+            </div>
+          ) : null}
+          {productCategory === "new category" || (!categories.includes(productCategory) && productCategory !== "") ?
+            (
+              <div>
+                <label htmlFor='productCategory'>Product category:</label>
+                <input
+                  type='text'
+                  id='productCategoryInput'
+                  name='productCategory'
+                  onChange={(e) => setProductCategory(e.target.value)}
+                  required
+                />
+              </div>
+            ) : null
+          }
+          <br />
           <h3>Add the product to the current list</h3>
           <div className='center_button'>
             <button type="submit">Add</button>
